5ce68033eb71dcd67812529babb4d8d95570eeee
Daniel San Juan <Daniel San Juan>
2017-02-12 18:48:59 +0000

    Main issue finished. Working on new features

--- app/src/main/java/com/example/danisj/docapp/MainActivity.java
+++ app/src/main/java/com/example/danisj/docapp/MainActivity.java
@@ -3,37 +3,35 @@ package com.example.danisj.docapp;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Parcelable;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.text.SpannableString;
-import android.text.style.UnderlineSpan;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.lowagie.text.Cell;
 import com.lowagie.text.Chunk;
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
-import com.lowagie.text.Element;
-import com.lowagie.text.PageSize;
+import com.lowagie.text.Font;
 import com.lowagie.text.Paragraph;
 import com.lowagie.text.Phrase;
-import com.lowagie.text.pdf.PdfCell;
-import com.lowagie.text.pdf.PdfImportedPage;
 import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
@@ -42,11 +40,15 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     private final static String NOMBRE_CARPETA_APP = "com.example.danisj.docapp";
     private final static String GENERADOS = "MisArchivos";
+    private String fileName = "dietaPacientes";
     private Document document = new Document();
     private PdfPTable table = new PdfPTable(2);
+    private File outputFile;
     private EditText floorNumber, blockNumber, firstPlate, secondPlate, portion, dessert;
     private Spinner day, time;
     private ArrayAdapter<String> adapter;
+    private List<EditText> editTextList;
+    private int numberOfFile = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +56,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         setContentView(R.layout.activity_main);
 
         initFields();
+        createEditTextList();
         initSpinners();
 
         findViewById(R.id.btnGenerar).setOnClickListener(this);
@@ -90,6 +93,16 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         day = (Spinner) findViewById(R.id.spinnerDay);
         time = (Spinner) findViewById(R.id.spinnerHourOfFood);
         table.setWidthPercentage(80);
+        editTextList = new ArrayList<>();
+    }
+
+    private void createEditTextList() {
+        editTextList.add(floorNumber);
+        editTextList.add(blockNumber);
+        editTextList.add(firstPlate);
+        editTextList.add(secondPlate);
+        editTextList.add(portion);
+        editTextList.add(dessert);
     }
 
     private void onClickExitButton() {
@@ -100,39 +113,61 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         } catch (DocumentException e) {
             e.printStackTrace();
         }
+        showPdfFile(outputFile.getAbsolutePath());
     }
 
     private void onClickSaveButton() {
-        table.addCell(floorNumber.getText() + "                                     " + blockNumber.getText() +
-                "               " + day.getSelectedItem().toString() + "/" + time.getSelectedItem().toString() + "\n\n\n" +
-                "               " + firstPlate.getText() + "\n\n" +
-                "               " + secondPlate.getText() + "\n\n" +
-                "               " + "c/" + portion.getText() + "\n\n" +
-                "               " + dessert.getText() + "\n\n\n\n\n\n\n");
+        PdfPCell pdfPCell = new PdfPCell();
+        pdfPCell.addElement(new Paragraph(floorNumber.getText().toString() + "                                      " + blockNumber.getText().toString(), new Font(2, 14, 1)));
+        pdfPCell.addElement(new Paragraph("               " + day.getSelectedItem().toString() + "/" + time.getSelectedItem().toString(), new Font(2, 14, 1)));
+        pdfPCell.addElement(new Paragraph("\n"));
+        pdfPCell.addElement(new Paragraph("               " + firstPlate.getText().toString(), new Font(2, 14, 1)));
+        pdfPCell.addElement(new Paragraph("\n"));
+        pdfPCell.addElement(new Paragraph("               " + secondPlate.getText().toString(), new Font(2, 14, 1)));
+        pdfPCell.addElement(new Paragraph("\n"));
+        if (portion.getText().length() != 0) {
+            pdfPCell.addElement(new Paragraph("               " + "c/" + portion.getText().toString(), new Font(2, 14, 1)));
+            pdfPCell.addElement(new Paragraph("\n"));
+        }
+        pdfPCell.addElement(new Paragraph("                 " + dessert.getText().toString(), new Font(2, 14, 1)));
+        pdfPCell.addElement(new Paragraph("\n\n\n"));
+        table.addCell(pdfPCell);
+        Toast.makeText(getApplicationContext(), "Se han guardado con exito los datos del paciente", Toast.LENGTH_SHORT).show();
+        clearFields();
+    }
+
+    private void clearFields() {
+        for (int i = 0; i < editTextList.size(); i++) {
+            editTextList.get(i).getText().clear();
+        }
     }
 
     @Override
     public void onClick(View v) {
-        System.out.println("Evento lanzado");
-        String fileName = "MiArchivoPDF.pdf";
         String tarjetaSD = Environment.getExternalStorageDirectory().toString();
         File pdfDir = new File(tarjetaSD + File.separator + NOMBRE_CARPETA_APP);
 
-        if (!pdfDir.exists()){
+        if (!pdfDir.exists()) {
             pdfDir.mkdir();
-            System.out.println("Directorio creado");
         }
 
         File subDir = new File(pdfDir.getPath() + File.separator + GENERADOS);
-        if (!subDir.exists()){
+        if (!subDir.exists()) {
             subDir.mkdir();
-            System.out.println("SubDirectorio creado");
         }
 
         String fullPath = Environment.getExternalStorageDirectory() + File.separator + NOMBRE_CARPETA_APP +
-                File.separator + GENERADOS + File.separator + fileName;
+                File.separator + GENERADOS + File.separator;
 
-        File outputFile = new File(fullPath);
+
+        outputFile = new File(fullPath + fileName + numberOfFile + ".pdf");
+
+        if (outputFile.exists()){
+            while (outputFile.exists()){
+                numberOfFile += 1;
+                outputFile = new File(fullPath + fileName + numberOfFile + ".pdf");
+            }
+        }
 
         try {
             FileOutputStream ficheroPdf = new FileOutputStream(outputFile.getAbsolutePath());
@@ -165,147 +200,21 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         findViewById(R.id.saveData).setEnabled(true);
         findViewById(R.id.closePdf).setEnabled(true);
     }
-}
-/*package com.example.danisj.docapp;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
+    public void showPdfFile(String fileName){
 
-import android.os.Build;
-import android.os.Bundle;
-import android.os.Environment;
-import android.support.annotation.RequiresApi;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+        File file = new File(fileName);
 
-import com.lowagie.text.Cell;
-import com.lowagie.text.Chunk;
-import com.lowagie.text.Document;
-import com.lowagie.text.DocumentException;
-import com.lowagie.text.Element;
-import com.lowagie.text.PageSize;
-import com.lowagie.text.Paragraph;
-import com.lowagie.text.Phrase;
-import com.lowagie.text.pdf.PdfCell;
-import com.lowagie.text.pdf.PdfImportedPage;
-import com.lowagie.text.pdf.PdfPCell;
-import com.lowagie.text.pdf.PdfPTable;
-import com.lowagie.text.pdf.PdfWriter;
-
-public class MainActivity extends AppCompatActivity implements OnClickListener {
-
-    private final static String NOMBRE_CARPETA_APP = "com.example.danisj.docapp";
-    private final static String GENERADOS = "MisArchivos";
-    private Document document = new Document();
-    private PdfPTable table = new PdfPTable(2);
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        table.setWidthPercentage(80);
-        final EditText editText = (EditText) findViewById(R.id.blockNumber);
-        Button saveButton = (Button) findViewById(R.id.saveButton);
-        saveButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                table.addCell(addCell());
-                /*table.addCell("Floor                              Block\n           Test1\n\n\n" +
-                        "\n\n" +
-                        "\n\n" + "Test2" +  "\n\n\n\n\n\n\n"
-                + "Test3" + "\n\n\n" + editText.getText());
-}
-        });
-        Button exitButton = (Button) findViewById(R.id.closeFile);
-        exitButton.setOnClickListener(new OnClickListener() {
-@Override
-public void onClick(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.fromFile(file),"application/pdf");
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         try {
-        table.addCell("");
-        document.add(table);
-        } catch (DocumentException e) {
-        e.printStackTrace();
+            this.startActivity(intent);
         }
-        document.close();
+        catch (ActivityNotFoundException e) {
+            Toast.makeText(this, "No Application Available to View PDF", Toast.LENGTH_SHORT).show();
         }
-        });
-        findViewById(R.id.btnGenerar).setOnClickListener(this);
-        }
-
-@Override
-public void onClick(View v) {
-        String[] permissions = {
-                "android.permission.READ_EXTERNAL_STORAGE",
-                "android.permission.WRITE_EXTERNAL_STORAGE"
-        };
-        int requestCode = 200;
-        requestPermissions(permissions, requestCode);
-        String estado = Environment.getExternalStorageState();
-        boolean sdDisponible = false;
-        boolean sdAccesoEscritura = false;
-        if (estado.equals(Environment.MEDIA_MOUNTED))
-        {
-        sdDisponible = true;
-        sdAccesoEscritura = true;
-        }
-        else if (estado.equals(Environment.MEDIA_MOUNTED_READ_ONLY))
-        {
-        sdDisponible = true;
-        sdAccesoEscritura = false;
-        }
-        else
-        {
-        sdDisponible = false;
-        sdAccesoEscritura = false;
-        }
-
-        System.out.println("Esta disponible: " + sdDisponible);
-        System.out.println("Se puede escribir: " + sdAccesoEscritura);
-
-
-        String fileName = "MiArchivoPDF.pdf";
-        String tarjetaSD = Environment.getExternalStorageDirectory().toString();
-        File pdfDir = new File(tarjetaSD + File.separator + NOMBRE_CARPETA_APP);
-
-        if (!pdfDir.exists()){
-        pdfDir.mkdir();
-        }else{
-        System.out.println("EXISTE EL DIRECTORIO");
-        }
-
-        File subDir = new File(pdfDir.getPath() + File.separator + GENERADOS);
-        if (!subDir.exists()){
-        subDir.mkdir();
-        }else{
-        System.out.println("EXISTE EL SUBDIRECTORIO");
-        }
-
-
-        String fullPath = Environment.getExternalStorageDirectory() + File.separator + NOMBRE_CARPETA_APP +
-        File.separator + GENERADOS + File.separator + fileName;
-
-        File outputFile = new File(fullPath);
-
-        try {
-        FileOutputStream ficheroPdf = new FileOutputStream(outputFile.getAbsolutePath());
-
-        PdfWriter.getInstance(document, ficheroPdf);
-
-        document.open();
-
-        } catch (DocumentException | FileNotFoundException e) {
-        e.printStackTrace();
-        }
-        }
-
-private PdfPCell addCell() {
-        return new PdfPCell(new Phrase("Hola"));
-        }
-        }
-        */
+        finish();
+    }
+}
